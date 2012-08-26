d5c1d4402192b36e9bf5d77f7b15a3e9d481feb4
Michael Steinert <mike.steinert@gmail.com>
2012-08-25 20:16:48 -0600

    Start "Higher Order Functions"
    Signed-off-by: Michael Steinert <mike.steinert@gmail.com>

---
+++ hhfuns.erl
@@ -0,0 +1,13 @@
+-module(hhfuns).
+-compile(export_all).
+
+one() -> 1.
+two() -> 2.
+
+add(X,Y) -> X() + Y().
+
+increment([]) -> [];
+increment([H|T]) -> [H+1|increment(T)].
+
+decrement([]) -> [];
+decrement([H|T]) -> [H-1|decrement(T)].
